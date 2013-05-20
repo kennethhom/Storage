1f555642753bed39d13635395dc40f5ab7c52b3a
Aviv Kiss <avivkiss@gmail.com>
2013-05-20 10:06:04 -0700

    Added constants

--- WineHipster/src/com/example/winehipster/EntryDatabaseHelper.java
+++ WineHipster/src/com/example/winehipster/EntryDatabaseHelper.java
@@ -15,6 +15,26 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class EntryDatabaseHelper extends SQLiteOpenHelper {
 	public static final String DB_NAME = "entries.sqlite";
 	public static final int VERSION = 1;
+	
+	public static final String TABLE_ENTRY = "entry";
+	public static final String COLUMN_ID = "_id";
+	public static final String COLUMN_DRAFT = "is_draft";
+	public static final String COLUMN_WINE = "wine";
+	public static final String COLUMN_TYPE = "type";
+	public static final String COLUMN_COUNTRY_REGION = "country_region";
+	public static final String COLUMN_SERVING_TEMPERATURE = "serving_temperature";
+	public static final String COLUMN_APPEARENCE = "appearence";
+	public static final String COLUMN_AROMA = "aroma";
+	public static final String COLUMN_TASTE = "taste";
+	public static final String COLUMN_PAIRED_WITH = "paired_with";
+	public static final String COLUMN_OPINION = "opinion";
+	public static final String COLUMN_VINTAGE = "vintage";
+	public static final String COLUMN_RATING = "rating";
+	public static final String COLUMN_PRICE = "price";
+	public static final String COLUMN_ALCOHOL_CONTENT = "alcohol_content";
+	public static final String COLUMN_PHOTO = "photo";
+	public static final String COLUMN_ENTRY_LOCATION = "location";
+	public static final String COLUMN_ENTRY_DATE = "entry_date";
 
 	public EntryDatabaseHelper(Context context) {
 		super(context, DB_NAME, null, VERSION);
@@ -25,7 +45,7 @@ public class EntryDatabaseHelper extends SQLiteOpenHelper {
 	 * @see android.database.sqlite.SQLiteOpenHelper#onCreate(android.database.sqlite.SQLiteDatabase)
 	 */
 	@Override
-	public void onCreate(SQLiteDatabase arg0) {
+	public void onCreate(SQLiteDatabase db) {
 		// TODO Auto-generated method stub
 
 	}
